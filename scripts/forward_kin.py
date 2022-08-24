040f13190aca1b74cf9be5d415c4dd1e3f01cf7e
Kaushik <narayankaushik49@gmail.com>
2022-08-24 21:33:36 +0530

    Debug

--- scripts/forward_kin.py
+++ scripts/forward_kin.py
@@ -1,25 +1,32 @@
 import numpy as np
-from numpy import *
+from numpy import array
 import math
 
-# Joint angles 
-t1=math.pi/2
-t2=0
-t3=math.pi/2
-t4=0
-t5=math.pi
 
-sin=math.sin
-cos=math.cos
-# DH parameter matrix [theta d a {alpha}] 
-# parameters for Alpha 2 arm 5 DOF
+sin=float(math.sin)
+cos=float(math.cos)
+pi=np.pi
+
+print(type(sin))
+
+# Joint angles 
+
+t1=pi/2
+t2=-pi/2
+t3=pi/2
+t4=0
+t5=pi/2
+t6=0
+
+# DH parameter matrix [theta d a {alpha}] for Intelledex 660T
 
 DH_matrix= np.array([
-    [t1,215,0,-math.pi/2],
-    [t2,0,177.8,0],
-    [t3,0,177.8,0],
-    [t4,0,0,-math.pi/2],
-    [t5,129.5,0,0]
+    [t1,373.4,0,pi/2],
+    [t2,0,0,pi/2],
+    [t3,0,304.8,0],
+    [t4,0,304.8,0],
+    [t5,0,0,pi/2],
+    [t6,228.6,0,0]
     ])
 
 # Transformation matrix for frame k-1 to k
@@ -41,11 +48,14 @@ def Calculate_Position_matrix():
                         [0 , 0 , 0 , 1]
                         
                         ]) 
+        print(temp_matrix)
+        print('\n')
+        
 
         Transformation_matrix= MultiplyMatrix( Transformation_matrix , temp_matrix )
     
-    print(Transformation_matrix)
- 
+    # print(Transformation_matrix)
+
  
     
     
@@ -58,6 +68,7 @@ def MultiplyMatrix(A,B):
 
     result = np.dot(A,B)
 
+    # print(result)
     return result
 
 
