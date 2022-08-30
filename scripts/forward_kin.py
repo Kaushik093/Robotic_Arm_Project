de3836c07ca4e0e43ba4d6123022413370fec9fb
Kaushik Narayan <narayankaushik49@gmail.com>
2022-08-30 16:45:21 +0530

    Completed forward kinematics

--- scripts/forward_kin.py
+++ scripts/forward_kin.py
@@ -1,4 +1,6 @@
-from itertools import count
+# Python code for computing forward kinematics for any robot with any number of DOF
+
+
 import numpy as np
 import math
 
@@ -11,11 +13,13 @@ pi=np.pi
 t1=pi/2
 t2=-pi/2
 t3=pi/2
-t4=0
+t4=pi
 t5=pi/2
 t6=0
 
 # DH parameter [theta d a {alpha}] for Intelledex 660T
+
+# Change the DH parameters to get the forward kinematics for any robot
 
 theta=[t1,t2,t3,t4,t5,t6]
 d = [373.4,0,0,0,0,228.4]
@@ -54,27 +58,46 @@ def Calculate_matrix():
 
     position_vector = [Final_matrix[0][3] , Final_matrix[1][3] , Final_matrix[2][3]]
 
+    print("Position: ",position_vector,"\n")
+
     euler_from_matrix(Final_matrix)
 
+
 def euler_from_matrix(Final_matrix):
-
-    orientation_vector=np.zeros(3)
-
 
     orientation_vector = np.array([[Final_matrix[0][0],Final_matrix[0][1],Final_matrix[0][2]],     # Extracting orientation vector from Final_matrix
                                     [Final_matrix[1][0],Final_matrix[1][1],Final_matrix[1][2]],
                                     [Final_matrix[2][0],Final_matrix[2][1],Final_matrix[2][2]],
-                                    ],dtype=float)                                                      
-    
-    
-    
-    pitch= - np.arcsin(Final_matrix[2][0])
-    roll = np.arctan2(Final_matrix[2][1] , Final_matrix[2][2])
-    yaw= np.arctan2(Final_matrix[1][0]/ float(cos(pitch)) , Final_matrix[0][0]/float(cos(theta)))
-    print(pitch," ",roll,)   
+                                    ],dtype=float)  
 
-    # print(orientation_vector)
+    # Extracting Euler angles from orientation vector
 
+    if(orientation_vector[2][0]!=1):
+        pitch1=-math.asin(orientation_vector[2][0]) 
+        pitch2= pi - pitch1
+
+        roll1 = math.atan2(orientation_vector[2][1]/math.cos(pitch1),orientation_vector[2][2]/math.cos(pitch1))
+        roll2 = math.atan2(orientation_vector[2][1]/math.cos(pitch2),orientation_vector[2][2]/math.cos(pitch2))
+
+        yaw1 = math.atan2(orientation_vector[1][0]/math.cos(pitch1),orientation_vector[0][0]/math.cos(pitch1))
+        yaw2 = math.atan2(orientation_vector[1][0]/math.cos(pitch2),orientation_vector[0][0]/math.cos(pitch2))
+
+        print("Two solutions for orientation of end effector : \n")
+        print("Roll: ",roll1*180/pi," Pitch: ",pitch1*180/pi," Yaw: ",yaw1*180/pi,"\n")
+        print("Roll: ",roll2*180/pi," Pitch: ",pitch2*180/pi," Yaw: ",yaw2*180/pi)
+    else :
+        yaw=0  # can be set to any value
+        if(orientation_vector[2][0]==-1):
+            pitch = pi/2
+            roll = yaw + math.atan2(orientation_vector[0][1],orientation_vector[0][2])
+        else:
+            pitch = -pi/2
+            roll = -yaw + math.atan2(-orientation_vector[0][1],-orientation_vector[0][2])
+
+        print("Orientation of end effector : \n")
+        print("Roll: ",roll*180/pi," Pitch: ",pitch*180/pi," Yaw: ",yaw*180/pi)
+         
+    
 
 Calculate_matrix()
 
