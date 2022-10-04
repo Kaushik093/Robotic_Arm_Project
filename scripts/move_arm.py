2a718f210407e18fd1b3746c04b487eb5233d5e7
Kaushik <narayankaushik49@gmail.com>
2022-10-04 21:17:59 +0530

    Changes

--- scripts/move_arm.py
+++ scripts/move_arm.py
@@ -20,11 +20,12 @@ pose_target= geometry_msgs.msg.Pose()
 joint_angles=[]
 # Setting target pose for end effector
 
-# q = quaternion_from_euler(0,0,0) 
-pose_target.orientation.x= 0.6025924555050339
-pose_target.orientation.y= -0.3248326766835255
-pose_target.orientation.z= 0.2933271227926523
-pose_target.orientation.w = 0.667326954169
+q = quaternion_from_euler(0,0,0) 
+
+pose_target.orientation.x= q[0]
+pose_target.orientation.y= q[1]
+pose_target.orientation.z= q[2]
+pose_target.orientation.w = q[3]
 
 pose_target.position.x = 0.20754018659960138
 pose_target.position.y = 2.271022464834049e-06
@@ -40,12 +41,13 @@ group.go(wait=True)   # Execute the plan and wait for completion
 
 group.stop()      #Stop the robot
 
+for i in range(len(group.get_current_joint_values())):
+    joint_angles.append(group.get_current_joint_values()[i]*180/math.pi)    #Get the current joint angles in degrees
+print("joint angles in degrees : ",joint_angles)
+
 group.clear_pose_targets()   #Clear the target
 
 moveit_commander.roscpp_shutdown()
 
-# for i in range(len(group.get_current_joint_values())):
-#     joint_angles.append(group.get_current_joint_values()[i]*180/math.pi)
-# print("joint angles in degrees : ",joint_angles)
 
 # Move it cmdline interface : rosrun moveit_commander moveit_commander_cmdline.py
