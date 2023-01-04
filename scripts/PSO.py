508cd309ffac45bea407533787d5a090650cb62e
Kaushik Narayan <narayankaushik49@gmail.com>
2023-01-04 16:05:07 +0530

    PSO finished

--- scripts/PSO.py
+++ scripts/PSO.py
@@ -1,69 +1,51 @@
-# PSO technique to optimise the path followed by the ur5 robot
-from move_arm_UR5 import get_path
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
 from pyswarm import pso
 import numpy as np
-from geometry_msgs.msg import Pose
-from moveit_msgs.msg import DisplayTrajectory
+from move_arm_UR5 import get_path
 
-robot = get_path()
-robot.move()
-path = robot.get_path()
-# print(path)
-
+count =0
 
 def forward_kinematics(joint_angles):
-    # Define the DH parameters for the UR5e arm
-    DH_params = [
-        {'a': 0, 'alpha': np.pi/2, 'd': 0.089159, 'theta': joint_angles[0]},
-        {'a': -0.425, 'alpha': 0, 'd': 0, 'theta': joint_angles[1] - np.pi/2},
-        {'a': -0.39225, 'alpha': 0, 'd': 0, 'theta': joint_angles[2]},
-        {'a': 0, 'alpha': np.pi/2, 'd': 0.10915, 'theta': joint_angles[3]},
-        {'a': 0, 'alpha': -np.pi/2, 'd': 0.09465, 'theta': joint_angles[4]},
-        {'a': 0, 'alpha': 0, 'd': 0.0823, 'theta': joint_angles[5]}
-    ]
+    # print("Joint angles: ",joint_angles)
+    # Define the DH parameters for the arm
+    # Each row represents a link and contains the following parameters:
+    #  [alpha, a, d, theta]
+    dh_params = np.array([[0, 0, 0, joint_angles[0]],
+                         [np.pi/2, 0, 0, joint_angles[1]],
+                         [0, 0.425, 0, joint_angles[2]],
+                         [0, 0.39225, 0, joint_angles[3]],
+                         [np.pi/2, 0, 0, joint_angles[4]],
+                         [-np.pi/2, 0, 0, joint_angles[5]]])
 
-    # Initialize the transformation matrix
-    T = np.eye(4)
+    # Initialize the transformation matrix for the base frame
+    T = np.identity(4)
 
-    # Compute the forward kinematics using the DH convention
-    for i, param in enumerate(DH_params):
-        a = param['a']
-        alpha = param['alpha']
-        d = param['d']
-        theta = param['theta']
-        T_i = np.array([
-            [np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
-            [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
-            [0, np.sin(alpha), np.cos(alpha), d],
-            [0, 0, 0, 1]
-        ])
-        T = T @ T_i
+    # Compute the transformation matrices for each link
+    for i in range(dh_params.shape[0]):
+        alpha, a, d, theta = dh_params[i]
+        T_link = np.array([[np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
+                           [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
+                           [0, np.sin(alpha), np.cos(alpha), d],
+                           [0, 0, 0, 1]])
+        T = T @ T_link
 
-    # Extract the position of the end effector from the transformation matrix
-    x, y, z = T[:3, 3]
-    return np.array([x, y, z])
+    # Return the position and orientation of the end effector frame
+    end_effector_pose = T[:3, 3]
+    end_effector_orientation = T[:3, :3]
+    return end_effector_pose, end_effector_orientation
 
-
-def optimize_path(path):
-    # Set the bounds for the PSO algorithm
-    bounds = [(0, 2*np.pi)] * 6 * len(path)
-
-    # Set the initial guess for the PSO algorithm
-    x0 = initial_joint_angles * len(path)
-
-    # Call the PSO algorithm to compute the optimal joint angles
-    xopt, fopt = pso(objective_function, bounds, x0, maxiter=100, swarmsize=50)
-
-    # Return the optimal joint angles
-    print(xopt)
 
 def objective_function(x):
+
+    global count
+    
     # Convert the joint angles to a joint state message
     joint_state = JointState()
     joint_state.position = x
+    
+    # print(len(x))
 
     # Compute the forward kinematics for the arm
     end_effector_poses = [forward_kinematics(joint_state.position)]
@@ -71,22 +53,47 @@ def objective_function(x):
         joint_state.position = x[i*6:(i+1)*6]
         end_effector_poses.append(forward_kinematics(joint_state.position))
 
+    
     # Compute the total distance traveled by the end effector along the path
     distance = 0
     for i in range(1, len(path)):
-        distance += np.linalg.norm(end_effector_poses[i] - end_effector_poses[i-1])
+        # print("Pose 1: {}".format(end_effector_poses[i][0]))
+        # print("Pose 2: {}".format(end_effector_poses[i-1][0]))
+        distance += np.linalg.norm(end_effector_poses[i][0] - end_effector_poses[i-1][0])
+    
+    print("Distance: {}".format(distance))
+   
+
     return distance
 
 
-if __name__ == '__main__':
-    # Initialize the ROS node
-    rospy.init_node('PSO')
+def generate_trajectory(path):
+    # Set the bounds for the PSO algorithm
+    lower_bounds = [(0)] * 6 * len(path)
+    upper_bounds = [(2*np.pi)] * 6 * len(path)
 
-    # Set the initial joint angles
-    global initial_joint_angles
-    initial_joint_angles = [0, 0, 0, 0, 0, 0]
+    # Set the initial guess for the PSO algorithm
+    x0 = initial_joint_angles * len(path)
 
-    # Create publishers for the joint angle topics
-    global joint_angle_publishers
+    # Call the PSO algorithm to compute the optimal joint angles
+    xopt, fopt = pso(objective_function, lower_bounds, upper_bounds,  maxiter=100, swarmsize=50)   #fopt is the minimum distance 
 
-    optimize_path(path)
+    print("Optimal joint angles: {}".format(xopt))
+    return xopt
+
+robot = get_path()  
+robot.move()
+path = robot.get_path()   #Initial path to be optimized
+
+
+initial_joint_angles = [0, 0, 0, 0, 0, 0]   #Initial joint angles
+
+joint_names = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+
+rospy.init_node("PSO")
+
+trajectory = generate_trajectory(path)
+
+
+# pub = rospy.Publisher("joint_states", JointState, queue_size=10)
+# pub.publish(trajectory)
--- scripts/move_arm_UR5.py
+++ scripts/move_arm_UR5.py
@@ -14,26 +14,37 @@ class get_path:
 
     def move(self):
         q = quaternion_from_euler(0, 0, 0)
+
         self.pose_target.orientation.x = q[0]
         self.pose_target.orientation.y = q[1]
         self.pose_target.orientation.z = q[2]
         self.pose_target.orientation.w = q[3]
-        self.pose_target.position.x = -0.7
-        self.pose_target.position.y = -0.2
-        self.pose_target.position.z = 0.3
-        self.group.set_pose_target(self.pose_target)
-        self.group.set_planner_id("RRTstarkConfigDefault")
-        plan = self.group.plan()
-        self.group.go(wait=True)
-        for pose in plan[1].joint_trajectory.points:
-            position = pose.positions
-            self.path.append(position)    # path is a list of joint angles at each point
 
-        self.group.stop()
+        self.pose_target.position.x =   -0.4
+        self.pose_target.position.y = -0.1192
+        self.pose_target.position.z = 0.48778
+
+        self.group.set_pose_target(self.pose_target)
+
+        self.group.set_start_state_to_current_state()
+        
+        self.group.set_planner_id("RRTkConfigDefault")
+
+        plan = self.group.plan()
+        
+        if plan[0]==True : 
+            for pose in plan[1].joint_trajectory.points:
+                position = pose.positions
+                self.path.append(position) 
+                self.group.go(wait=True)
+        else :
+            print("No path found")
+            self.group.stop()
+            
+        
         self.group.clear_pose_targets()
     def get_path(self):
         return self.path
 
-rospy.init_node("move_group_python", anonymous=True)
 
 # moveit_commander.roscpp_shutdown()
